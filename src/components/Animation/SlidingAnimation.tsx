5765f72908cc1d85e863418d230bcfdef6cc7b24
YonteMoon <monteyoon1998@gmail.com>
2024-09-12 14:47:08 -0700

    Animation now works when tabbed out or inactive

--- src/components/Animation/SlidingAnimation.tsx
+++ src/components/Animation/SlidingAnimation.tsx
@@ -1,19 +1,39 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { generateRandomInteger } from "../../helper/utils";
+import { useEffect, useState } from "react";
 
 const SlidingAnimation = ({ children }: { children: React.ReactNode }) => {
   const randomInt = generateRandomInteger();
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      console.log(document.visibilityState);
+      if (document.visibilityState === "visible") {
+        setIsActive(true);
+      } else {
+        setIsActive(false);
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        key={randomInt}
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: 20 }}
-        transition={{ duration: 0.5, type: "spring" }}
-      >
-        {children}
-      </motion.div>
+      {isActive && (
+        <motion.div
+          key={randomInt}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 20 }}
+          transition={{ duration: 0.5, type: "spring" }}
+        >
+          {children}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 };
